c2533a3ea011d7194a6b33dd391641cc147e3f9b
John Doe <john@sample.com>
2019-10-29 19:38:27 +0800

    new

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from app.view import user,uitest,utils,minder,minderfiles
+
+app = Flask(__name__)
+app.config.from_object('config')
+bootstrap = Bootstrap(app)
+app.register_blueprint(user.mod)
+app.register_blueprint(uitest.mod)
+app.register_blueprint(utils.mod)
+app.register_blueprint(minder.mod)
+app.register_blueprint(minderfiles.mod)
+from app import views
